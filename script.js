96cc8497421a23806b3ea15c56842a5ae4acb00a
Tarik <tarikmerrylees@gmail.com>
2023-03-21 22:56:50 -0400

    script elements

---
+++ script.js
@@ -0,0 +1,17 @@
+// speed control
+const speedDec = document.getElementById("speedDec")
+const speedInc = document.getElementById("speedInc")
+const currentSpeed = document.getElementById("currentSpeed")
+
+// Player controls
+const stopBtn = document.getElementById("stopBtn")
+const PauseBtn = document.getElementById("PauseBtn")
+const PlayBtn = document.getElementById("PlayBtn")
+
+// misc Control
+const decFontBtn = document.getElementById("decFontBtn")
+const currentFontSize = document.getElementById("currentFontSize")
+const IncFontBtn = document.getElementById("IncFontBtn")
+
+const script = document.getElementById("script")
+
