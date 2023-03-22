2d11ee35c7ba9d81eb8151afc23a4869057ef15a
Tarik <tarikmerrylees@gmail.com>
2023-03-21 23:12:54 -0400

    make speed buttons change speed number

--- script.js
+++ script.js
@@ -2,17 +2,28 @@
 const speedDec = document.getElementById("speedDec")
 const speedInc = document.getElementById("speedInc")
 const currentSpeed = document.getElementById("currentSpeed")
+let speed = 1
 
 // Player controls
 const stopBtn = document.getElementById("stopBtn")
-const PauseBtn = document.getElementById("PauseBtn")
-const PlayBtn = document.getElementById("PlayBtn")
+const pauseBtn = document.getElementById("pauseBtn")
+const playBtn = document.getElementById("playBtn")
 
 // misc Control
 const decFontBtn = document.getElementById("decFontBtn")
 const currentFontSize = document.getElementById("currentFontSize")
-const IncFontBtn = document.getElementById("IncFontBtn")
+const incFontBtn = document.getElementById("incFontBtn")
 
 const script = document.getElementById("script")
 
 
+// change speed 
+speedInc.addEventListener("click", () => {
+  if (speed < 20) speed += 1
+  currentSpeed.innerText = `x${speed}`
+})
+
+speedDec.addEventListener("click", () => {
+  if (speed > 1) speed -= 1
+  currentSpeed.innerText = `x${speed}`
+})
